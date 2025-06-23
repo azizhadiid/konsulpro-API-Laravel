49f664f8ddccd2a9b45e53f9c9f030247451f6d3
azizhadiid <azizalhadiid88@gmail.com>
2025-06-23 20:24:29 +0700

    Day 1

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/ping', function () {
+    return response()->json([
+        'message' => 'API Laravel aktif 🚀'
+    ]);
+});
